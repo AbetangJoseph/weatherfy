935bea5f5a06b146bf240bd939fb9300f735b377
Abetang Joseph <josephabetang@gmail.com>
2019-08-09 17:38:37 +0100

    import boostrap, weather-icons and weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'weather-icons/css/weather-icons.css';
 
 import './App.css';
+import Weather from './components/weather';
 
 function App() {
   return (
     <div className="App">
-      <h1>weatherfy</h1>
+      <Weather />
     </div>
   );
 }
