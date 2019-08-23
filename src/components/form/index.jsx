b960e35f6faa7e657a3d702235c4f87aaec47048
Abetang Joseph <josephabetang@gmail.com>
2019-08-23 01:45:04 +0100

    add loadWeather, change, inputValue props

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import Input from '../input';
 import Button from '../button';
-import './style.css'
+import './style.css';
 
-export default function Form() {
+export default function Form({ loadWeather, change, inputValue }) {
   return (
-    <form>
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-md-3 offset-md-2">
-          <Input name="city" placeholder="City" autoComplete="off" />
-        </div>
-        <div className="col-md-3">
-          <Input name="country" placeholder="Country" autoComplete="off" />
-        </div>
-        <div className="col-md-3 mt-md-0 text-md-left">
-          <Button classes="btn btn-success" label="Get Weather" />
+    <form onSubmit={loadWeather}>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-3 offset-md-2">
+            <Input
+              name="city"
+              placeholder="City"
+              change={change}
+              autoComplete="off"
+              inputValue={inputValue.city}
+            />
+          </div>
+          <div className="col-md-3">
+            <Input
+              name="country"
+              placeholder="Country"
+              change={change}
+              autoComplete="off"
+              inputValue={inputValue.country}
+            />
+          </div>
+          <div className="col-md-3 mt-md-0 text-md-left">
+            <Button classes="btn btn-success" label="Get Weather" />
+          </div>
         </div>
       </div>
-    </div>
     </form>
   );
 }
